544261f00ddd1227039ad4180c797aaf091964fd
xXYeetMasterXx <39629379+xXYeetMasterXx@users.noreply.github.com>
2018-05-29 11:45:51 -0500

    Create A17.py

---
+++ A17.py
@@ -0,0 +1,35 @@
+states = {'Oregon': 'OR',
+          'Florida': 'FL',
+          'California': 'CA',
+          'New York': 'NY',
+          'Michigan': 'MI',
+          'Oklahoma': 'OK',
+          'Texas': 'TX'}
+cities = {'CA': 'San Francisco',
+          'MI': 'Detroit', 
+          'FL': 'Jacksonville'}  
+          
+#add the rest of the cities here including Oklahoma and Texas
+
+cities ['NY'] = 'New York'
+cities ['OR'] = 'Portland'
+cities ['OK'] = 'Moon'
+cities ['TX'] = 'Kermit'
+# Print out all the remaining cities
+print ('-' * 20)
+print ("NY State has: ", cities['NY'])
+print ("OR State has: ", cities['OR'])
+print ("OK State has: ", cities['OK'])
+print ("TX State has: ", cities['TX'])
+print ("CA State has: ", cities['CA'])
+print ("MI State has: ", cities['MI'])
+print ("FL State has: ", cities['FL'])
+# Print out all the states
+print ('-' * 20)
+print ("Michigan's abbreviation is: ", states['Michigan'])
+print ("Oklahoma's abbreviation is: ", states['Oklahoma'])
+print ("Oregon's abbreviation is: ", states['Oregon'])
+print ("Texas's abbreviation is:", states['Texas'])
+print ("California's abbreviation is:", states['California'])
+print ("New York's abbreviation is:", states['California'])
+print ("Florida's abbreviation is:", states['Florida'])
