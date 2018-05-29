4c4cf9ca0797c358084a0e823b2c12db2e4b278a
xXYeetMasterXx <39629379+xXYeetMasterXx@users.noreply.github.com>
2018-05-29 11:52:18 -0500

    Create A23.py

---
+++ A23.py
@@ -0,0 +1,8 @@
+def sum_three():
+  print ("This finds the sum of three numbers")
+  a = int(input("Enter your first number:"))
+  b = int(input("Enter your second number:"))
+  c = int(input("Enter your third number:"))
+  return (a+b+c)
+  
+print(sum_three())
